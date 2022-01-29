a3fecfb92fd575ea10d85a9b9c6777adc932e2f2
SnowballSH <derek070211@gmail.com>
2022-01-28 20:41:09 -0600

    Magic bitboards

--- src/main.zig
+++ src/main.zig
@@ -17,8 +17,8 @@ pub fn main() !void {
 
     // https://www.chessprogramming.org/Perft_Results
     // const s = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ";
-    // const s = Position.STARTPOS;
-    const s = "R1R1R1R1/7R/R7/3R3R/R4K2/2kp1R1R/R7/7R w - - 0 1";
+    const s = Position.STARTPOS;
+    // const s = "r3k2K/6P1/6N1/8/8/8/4p3/8 b q - 0 1";
     var pos = Position.new_position_by_fen(s);
     defer pos.deinit();
     pos.display();
@@ -28,5 +28,5 @@ pub fn main() !void {
     // FSF: 6.27s
     // SF: 0.84s
 
-    _ = try Perft.perft_root(&pos, 1);
+    _ = try Perft.perft_root(&pos, 6);
 }
