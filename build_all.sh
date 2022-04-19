b4d39b7d4d8100097032c1f84b5f309548035d17
SnowballSH <derek070211@gmail.com>
2022-04-18 22:47:41 -0500

    Update binaries

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 zig build -Drelease-fast=true -Dtarget=x86_64-windows
-cp ./zig-out/bin/Avalanche.exe ./binaries/Avalanche_x86_64_windows.exe
+mv ./zig-out/bin/Avalanche.exe ./binaries/Avalanche_x86_64_windows.exe
 zig build -Drelease-fast=true -Dtarget=x86_64-linux
-cp ./zig-out/bin/Avalanche ./binaries/Avalanche_x86_64_linux
+mv ./zig-out/bin/Avalanche ./binaries/Avalanche_x86_64_linux
 zig build -Drelease-fast=true -Dtarget=aarch64-macos
-cp ./zig-out/bin/Avalanche ./binaries/Avalanche_aarch64_macos
+mv ./zig-out/bin/Avalanche ./binaries/Avalanche_aarch64_macos
