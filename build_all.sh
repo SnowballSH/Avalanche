e725f6e7f921541b291432786751ab8de6f93a46
SnowballSH <derek070211@gmail.com>
2022-04-19 16:53:51 -0500

    Add Haswell windows binary

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,7 @@
 zig build -Drelease-fast=true -Dtarget=x86_64-windows
 mv ./zig-out/bin/Avalanche.exe ./binaries/Avalanche_x86_64_windows.exe
+zig build -Drelease-fast=true -Dtarget=x86_64-windows -Dcpu=haswell
+mv ./zig-out/bin/Avalanche.exe ./binaries/Avalanche_x86_64_windows_haswell.exe
 zig build -Drelease-fast=true -Dtarget=x86_64-linux
 mv ./zig-out/bin/Avalanche ./binaries/Avalanche_x86_64_linux
 zig build -Drelease-fast=true -Dtarget=aarch64-macos
