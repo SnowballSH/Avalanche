9f2a6305d9b2b0d17390c61f1b5b4679336358fb
SnowballSH <derek070211@gmail.com>
2022-04-27 07:28:05 -0500

    fix name

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,8 @@
 zig build -Drelease-fast=true -Dtarget=x86_64-windows
-mv ./zig-out/bin/Avalanche.exe ./binaries/Avalanche_x86_64_windows_0.3dev.exe
+mv ./zig-out/bin/Avalanche.exe ./binaries/Avalanche_x86_64_windows_0.2.2.exe
 zig build -Drelease-fast=true -Dtarget=x86_64-windows -Dcpu=haswell
-mv ./zig-out/bin/Avalanche.exe ./binaries/Avalanche_x86_64_windows_haswell_0.3dev.exe
+mv ./zig-out/bin/Avalanche.exe ./binaries/Avalanche_x86_64_windows_haswell_0.2.2.exe
 zig build -Drelease-fast=true -Dtarget=x86_64-linux
-mv ./zig-out/bin/Avalanche ./binaries/Avalanche_x86_64_linux_0.3dev
+mv ./zig-out/bin/Avalanche ./binaries/Avalanche_x86_64_linux_0.2.2
 zig build -Drelease-fast=true -Dtarget=aarch64-macos
-cp ./zig-out/bin/Avalanche ./binaries/Avalanche_aarch64_macos_0.3dev
+cp ./zig-out/bin/Avalanche ./binaries/Avalanche_aarch64_macos_0.2.2
