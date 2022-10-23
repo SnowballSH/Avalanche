fd2542bf0cd37dd62aa3062e4fc9895b724b71f5
SnowballSH <derek070211@gmail.com>
2022-10-23 11:17:38 -0500

    MTC tune

--- src/engine/parameters.zig
+++ src/engine/parameters.zig
@@ -1,12 +1,12 @@
-pub const LMRWeight = 0.647173874704215;
-pub const LMRBias = 1.301347679383754;
+pub const LMRWeight = 0.6351504771570033;
+pub const LMRBias = 1.2717388310225977;
 
-pub const RFPDepth = 5;
-pub const RFPMultiplier = 62;
+pub const RFPDepth = 4;
+pub const RFPMultiplier = 63;
 pub const RFPImprovingDeduction = 70;
 
 pub const NMPBase = 5;
 pub const NMPDepthDivisor = 5;
 pub const NMPBetaDivisor = 214;
 
-pub const RazoringMargin = 320;
+pub const RazoringMargin = 318;
