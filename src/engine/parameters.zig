14f8c92b1ab2f33981c0aa1fc63cca83d0a7f9d7
SnowballSH <derek070211@gmail.com>
2023-08-04 14:43:49 +0800

    update

--- src/engine/parameters.zig
+++ src/engine/parameters.zig
@@ -1,9 +1,9 @@
 pub var LMRWeight: f64 = 0.423;
 pub var LMRBias: f64 = 0.77;
 
-pub var RFPDepth: i32 = 5;
-pub var RFPMultiplier: i32 = 61;
-pub var RFPImprovingDeduction: i32 = 73;
+pub var RFPDepth: i32 = 8;
+pub var RFPMultiplier: i32 = 70;
+pub var RFPImprovingDeduction: i32 = 57;
 
 pub var NMPBase: usize = 3;
 pub var NMPDepthDivisor: usize = 3;
