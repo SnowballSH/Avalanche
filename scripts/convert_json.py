b3bb4e9a7d9d2779b3559408954a8ad4a95761c7
SnowballSH <derek070211@gmail.com>
2023-08-15 21:28:48 +0900

    net006_60

--- scripts/convert_json.py
+++ scripts/convert_json.py
@@ -6,7 +6,7 @@ import json
 import struct
 
 FEATURES = 768
-HIDDEN = 256
+HIDDEN = 256 + 128
 QA = 255
 QB = 64
 QAB = QA * QB
--- scripts/score_games.sh
+++ scripts/score_games.sh
@@ -5,10 +5,3 @@ python3 ./score_games.py ./games/t3 ./t3.txt & \
 python3 ./score_games.py ./games/t4 ./t4.txt & \
 python3 ./score_games.py ./games/t5 ./t5.txt & \
 wait)
-(trap 'kill 0' SIGINT; \
-python3 ./score_games.py ./games/t1 ./t1.txt & \
-python3 ./score_games.py ./games/t2 ./t2.txt & \
-python3 ./score_games.py ./games/t3 ./t3.txt & \
-python3 ./score_games.py ./games/t4 ./t4.txt & \
-python3 ./score_games.py ./games/t5 ./t5.txt & \
-wait)
