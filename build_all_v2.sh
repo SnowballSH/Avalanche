484464992d54bb295b3e2c38b503c16822778e0d
SnowballSH <derek070211@gmail.com>
2024-01-12 21:48:12 -0600

    release

--- build_all_v2.sh
+++ build_all_v2.sh
@@ -1,4 +1,4 @@
-VERSION="2.1.0-dev"
+VERSION="2.1.0"
 
 # Windows
 zig build -Dtarget=x86_64-windows -Drelease-fast -Dcpu=x86_64 --prefix artifacts/ -Dtarget-name="Avalanche-${VERSION}-x86_64-win-v1"
