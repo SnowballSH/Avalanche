e019c0721aae09d5671cc8f9b75979db776cc107
Yinuo Huang <yinuohuang@roblox.com>
2026-06-22 15:59:21 +0000

    Revert tuning

--- src/engine/parameters.zig
+++ src/engine/parameters.zig
@@ -1,19 +1,19 @@
-pub var LMRWeight: f64 = 0.490;
-pub var LMRBias: f64 = 0.778;
+pub var LMRWeight: f64 = 0.429;
+pub var LMRBias: f64 = 0.769;
 
-pub var RFPDepth: i32 = 9;
-pub var RFPMultiplier: i32 = 71;
-pub var RFPImprovingDeduction: i32 = 86;
+pub var RFPDepth: i32 = 8;
+pub var RFPMultiplier: i32 = 58;
+pub var RFPImprovingDeduction: i32 = 69;
 
-pub var NMPImprovingMargin: i32 = 86;
+pub var NMPImprovingMargin: i32 = 72;
 pub var NMPBase: usize = 3;
-pub var NMPDepthDivisor: usize = 2;
+pub var NMPDepthDivisor: usize = 3;
 pub var NMPBetaDivisor: i32 = 206;
 
-pub var RazoringBase: i32 = 85;
-pub var RazoringMargin: i32 = 212;
+pub var RazoringBase: i32 = 68;
+pub var RazoringMargin: i32 = 191;
 
-pub var AspirationWindow: i32 = 14;
+pub var AspirationWindow: i32 = 11;
 
 pub const Tunable = struct {
     name: []const u8,
@@ -26,16 +26,16 @@ pub const Tunable = struct {
 };
 
 pub const TunableParams = [_]Tunable{
-    Tunable{ .name = "LMRWeight", .value = "490", .min_value = "250", .max_value = "650", .c_end = "20", .r_end = "0.002", .id = 0 },
-    Tunable{ .name = "LMRBias", .value = "778", .min_value = "300", .max_value = "1300", .c_end = "50", .r_end = "0.002", .id = 1 },
-    Tunable{ .name = "RFPDepth", .value = "9", .min_value = "5", .max_value = "12", .c_end = "0.5", .r_end = "0.002", .id = 2 },
-    Tunable{ .name = "RFPMultiplier", .value = "71", .min_value = "30", .max_value = "110", .c_end = "4", .r_end = "0.002", .id = 3 },
-    Tunable{ .name = "RFPImprovingDeduction", .value = "86", .min_value = "20", .max_value = "120", .c_end = "5", .r_end = "0.002", .id = 4 },
-    Tunable{ .name = "NMPImprovingMargin", .value = "86", .min_value = "20", .max_value = "140", .c_end = "6", .r_end = "0.002", .id = 5 },
+    Tunable{ .name = "LMRWeight", .value = "429", .min_value = "250", .max_value = "650", .c_end = "20", .r_end = "0.002", .id = 0 },
+    Tunable{ .name = "LMRBias", .value = "769", .min_value = "300", .max_value = "1300", .c_end = "50", .r_end = "0.002", .id = 1 },
+    Tunable{ .name = "RFPDepth", .value = "8", .min_value = "5", .max_value = "12", .c_end = "0.5", .r_end = "0.002", .id = 2 },
+    Tunable{ .name = "RFPMultiplier", .value = "58", .min_value = "30", .max_value = "110", .c_end = "4", .r_end = "0.002", .id = 3 },
+    Tunable{ .name = "RFPImprovingDeduction", .value = "69", .min_value = "20", .max_value = "120", .c_end = "5", .r_end = "0.002", .id = 4 },
+    Tunable{ .name = "NMPImprovingMargin", .value = "72", .min_value = "20", .max_value = "140", .c_end = "6", .r_end = "0.002", .id = 5 },
     Tunable{ .name = "NMPBase", .value = "3", .min_value = "2", .max_value = "5", .c_end = "0.5", .r_end = "0.002", .id = 6 },
-    Tunable{ .name = "NMPDepthDivisor", .value = "2", .min_value = "2", .max_value = "5", .c_end = "0.5", .r_end = "0.002", .id = 7 },
+    Tunable{ .name = "NMPDepthDivisor", .value = "3", .min_value = "2", .max_value = "5", .c_end = "0.5", .r_end = "0.002", .id = 7 },
     Tunable{ .name = "NMPBetaDivisor", .value = "206", .min_value = "100", .max_value = "320", .c_end = "11", .r_end = "0.002", .id = 8 },
-    Tunable{ .name = "RazoringBase", .value = "85", .min_value = "20", .max_value = "140", .c_end = "6", .r_end = "0.002", .id = 9 },
-    Tunable{ .name = "RazoringMargin", .value = "212", .min_value = "80", .max_value = "320", .c_end = "12", .r_end = "0.002", .id = 10 },
-    Tunable{ .name = "AspirationWindow", .value = "14", .min_value = "5", .max_value = "30", .c_end = "1.25", .r_end = "0.002", .id = 11 },
+    Tunable{ .name = "RazoringBase", .value = "68", .min_value = "20", .max_value = "140", .c_end = "6", .r_end = "0.002", .id = 9 },
+    Tunable{ .name = "RazoringMargin", .value = "191", .min_value = "80", .max_value = "320", .c_end = "12", .r_end = "0.002", .id = 10 },
+    Tunable{ .name = "AspirationWindow", .value = "11", .min_value = "5", .max_value = "30", .c_end = "1.25", .r_end = "0.002", .id = 11 },
 };
